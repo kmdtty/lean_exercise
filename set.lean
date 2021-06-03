ab0f8e6c5436e0526d900288aa84f4ef5e520dee
Tatuya Kamada <kmdtty@users.noreply.github.com>
2021-06-03 14:12:21 +0900

    set.lean: Add proofs of idemponent laws of set on \cap and \cup

--- set.lean
+++ set.lean
@@ -5,19 +5,27 @@ variable {U : Type}
 variables A B C : set U
 variable x : U
 
--- not yet proved
+example : A ∩ A = A :=
+eq_of_subset_of_subset
+(assume x,
+ assume h: x ∈ (A ∩ A),
+ show x ∈ A, from and.left h)
+(assume x,
+ assume h: x ∈ A,
+ show x ∈ (A ∩ A), from and.intro h h)
+
 example : A ∪ A = A :=
 eq_of_subset_of_subset
 (assume x,
+ assume h: x ∈ A ∪ A,
+ have o1: x ∈ A ∨ x ∈  A, from h,
+ show x ∈ A, from or.elim o1
+ (assume a1: x ∈ A,
+  show x ∈ A, from a1)
+ (assume a2: x ∈ A,
+  show x ∈ A, from a2)
+)
+(assume x,
  assume h: x ∈ A,
  show x ∈ (A ∪ A),from or.inl h)
-(assume x,
- assume h: x ∈ (A ∪ A),
- have h1: x ∈ A ∨ x ∈ A, from h,
- or.elim h
- (assume hal: x ∉ A
-  show x ∈ A,from h1 hal)
- (assume har: x ∉ A,
-  show x ∈ A, from h1, har)
-
 
