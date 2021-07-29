79861bf16c26c1fcdcc7c7177dad5a8a59e31c92
Tatuya Kamada <kmdtty@users.noreply.github.com>
2021-07-29 14:33:34 +0900

    Update function.lean

--- function.lean
+++ function.lean
@@ -40,20 +40,67 @@ def injective (f : X → Y) : Prop :=
 def injective2 (f: X → Y) : Prop :=
  ∀ x₁ : X, ∀ x₂ :X, x₁ ≠ x₂ → f x₁ ≠ f x₂
 
+def injective3 (f: X → Y) : Prop :=
+ ∀ a a' : X, a ≠ a' → f a ≠ f a'
+
+def injective4 (f: X → Y) : Prop :=
+ ∀ a a' : X, f a = f a' → a = a'
+
 -- We can eliminate writing :Y, :X
 def surjective (f: X → Y) : Prop :=
  ∀ y, ∃ x, f x = y
 
 def bijective (f: X → Y) := injective f ∧ surjective f 
 
+/--
+ What is @id mean?
+ @ is for preventing to fill implicit argument 
+ and forcing to fill statement.
+ ... what?
+
+ If we get rid of @ from "@id", lean raises type
+ error anyway.
+--/
+
+/- 
+Let's prove identity function is injective.
+
+The syntax of injective proposition is defined with
+def injective (f: X → Y) : Prop 
+So "injective (@id X)" means, 
+given function id with domain X, it is injective.
+-/
 theorem injective_id : injective (@id X) :=
 assume x₁ x₂,
 assume H : id x₁ = id x₂,
-show x₁ = x₂, from H
+show x₁ = x₂, from H -- prove this from def of id
 
+theorem injective_id3 : injective3 (@id X) :=
+assume a a',
+assume H : a ≠ a',  -- contraposition can not be proved automatically
+show id a ≠ id a', from H 
+
+theorem injective_id4 : injective4 (@id X) :=
+assume a a',
+assume H : id a = id a',
+show a = a', from H 
+
+-- prove identity function is subjective
 theorem surjective_id : surjective (@id X) :=
 assume y,
 show ∃ x, id x = y, from exists.intro y rfl
 
+-- prove identy function is bijective 
 theorem bijective_id : bijective(@id X) :=
 and.intro injective_id surjective_id
+
+-- prove compisition of injective functions
+-- is injective
+
+theorem injective_comp {g : Y → Z} {f : X → Y}
+    (Hg : injective g) (Hf : injective f) :
+    injective (g ∘ f) :=
+assume x₁ x₂,
+assume : (g ∘ f) x₁ = (g ∘ f) x₂,
+have f x₁ = f x₂, from Hg this,
+show x₁ = x₂, from Hf this 
