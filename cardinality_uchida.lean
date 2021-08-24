737bb8b27327c2a825094816e2a2729497e1c1b0
Tatuya Kamada <kmdtty@users.noreply.github.com>
2021-08-24 21:57:17 +0900

    Add some checks on cardinaly_uchida

--- cardinality_uchida.lean
+++ cardinality_uchida.lean
@@ -1,5 +1,13 @@
+import data.set 
+open set 
+
 universe u
 variables {A B : Sort*}
+variables {α: Type u}
+#check set α
+#check α
+#check set
 
 def areIso (X Y: Type u) : Prop :=
   ∃ f : X → Y, ∃ g : Y → X, f ∘ g = @id Y ∧ g ∘ f = @id X
+
